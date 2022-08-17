ba3f291a795f4396d9d29c23cd8c0bdf7b5c1d0d
ahesm <ahesm@github.com>
2022-08-17 19:47:17 +0900

    path

--- site/site/wsgi.py
+++ site/site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for archivesite project.
+WSGI config for site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archivesite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site.settings')
 
 application = get_wsgi_application()
 
