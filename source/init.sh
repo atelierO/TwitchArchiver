6de1d52abc1a7e7ca16d93988e8991a32dbfb9ac
ahesm <ahesm@github.com>
2022-08-30 20:39:37 +0900

    init 분리

--- source/init.sh
+++ source/init.sh
@@ -1,3 +1,6 @@
-wget https://github.com/lay295/TwitchDownloader/releases/download/1.40.7/TwitchDownloaderCLI-Linux-x64.zip 
-unzip -o TwitchDownloaderCLI-Linux-x64.zip && chmod +x TwitchDownloaderCLI 
+ #!/bin/sh
+wget -N https://github.com/lay295/TwitchDownloader/releases/download/1.40.7/TwitchDownloaderCLI-Linux-x64.zip
+unzip -o TwitchDownloaderCLI-Linux-x64.zip
+rm TwitchDownloaderCLI-Linux-x64.zip
+chmod +x TwitchDownloaderCLI
 (./TwitchDownloaderCLI  --download-ffmpeg || true)
